008fff78101b0a3808c2cf109772fcf0937550aa
markclarus1990 <43287199+markclarus1990@users.noreply.github.com>
2024-12-07 11:55:32 +0800

    fix path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/freedomboard/", // This sets the base path for all assets and routing
+  base: "/freedomboard/", // This ensures the correct base path for GitHub Pages
 });
