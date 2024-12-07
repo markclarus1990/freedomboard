d52457f823d54001251698af641879b44acba471
markclarus1990 <43287199+markclarus1990@users.noreply.github.com>
2024-12-07 11:50:41 +0800

    added config to vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/freedomboard/", // This sets the base path for all assets and routing
+});
